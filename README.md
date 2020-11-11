059e26ad6a368bd539e659e9009f8d5491dd2a11
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-11-11 19:22:45 +0100

    First step to access rust code from java using early access panama code !

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Make it easy to access rs-handlegraph implementations in an other language.
 
+
+# java
+
+Uses early access build of project panama
+
+~/bin/jdk-16/bin/java -Dforeign.restricted=permit --add-modules jdk.incubator.foreign libhandlegraph-jni.java
+
+
---
+++ libhandlegraph-panama.java
@@ -0,0 +1,20 @@
+import java.lang.invoke.*;
+import jdk.incubator.foreign.*;
+import java.nio.file.Paths;
+
+class PanamaMain {
+  public static void main(String[] args) throws Throwable {
+    // get System linker
+    var linker = CLinker.getInstance();
+    var lookup = LibraryLookup.ofPath(Paths.get(System.getProperty("user.dir")+"/target/debug/librs_handlegraph_ffi.so"));
+
+    // get a native method handle for 'getpid' function
+    var getpid = linker.downcallHandle(
+           lookup.lookup("node_is_reverse").get(),
+           MethodType.methodType(boolean.class, MemoryAddress.class),
+           FunctionDescriptor.of(CLinker.C_BOOL, CLinker.C_POINTER));
+
+    // invoke it!
+    System.out.println((int)getpid.invokeExact());
+  }
+}
