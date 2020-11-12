9cf693a0ddb33357f35b02e79f9b3fa848e9cca9
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-11-12 18:15:58 +0100

    Take cff code and expand upon it

--- src/lib.rs
+++ src/lib.rs
@@ -1,152 +1,175 @@
-use handlegraph::handle::Handle;
-use handlegraph::handle::Edge;
-use handlegraph::handle::Direction;
-use handlegraph::handle::NodeId;
-use handlegraph::handlegraph::HandleGraph;
-use handlegraph::hashgraph::HashGraph;
-use handlegraph::pathgraph::PathHandleGraph;
-use handlegraph::mutablehandlegraph::MutableHandleGraph;
-use handlegraph;
+use handlegraph::{
+    handle::*, handlegraph::*, hashgraph::*, mutablehandlegraph::*, pathgraph::*,
+};
 
 use gfa::{
-    gfa::{Link, Segment, GFA},
+    gfa::GFA,
     optfields::OptFields,
     parser::{GFAParser, GFAResult},
 };
 
-#[no_mangle]
-pub extern fn node_as_integer(node: &Handle)-> u64 {
-    return node.as_integer();
+pub struct CGraph {
+    graph: HashGraph,
 }
 
-#[no_mangle]
-pub extern fn node_is_reverse(node: &Handle)-> bool {
-    return node.is_reverse();
+#[repr(C)]
+pub struct EdgeHolder{
+    left: u64,
+    right: u64,
 }
 
-#[no_mangle]
-pub extern fn node_id(node: &Handle)-> NodeId {
-    return node.id();
+#[repr(C)]
+pub struct HandlesIter<'a> {
+    finished: bool,
+    iter:  Box<dyn Iterator<Item = Handle> + 'a>,
 }
 
-// #[no_mangle]
-// pub extern fn edge_left(edge: &Edge)-> NodeId {
-//     return edge.;
-// }
-
-// #[no_mangle]
-// pub extern fn edge_right(edge: &Edge)-> NodeId {
-//     return edge.right;
-// }
-
-#[no_mangle]
-pub extern fn hash_graph_new() -> HashGraph {
-    return HashGraph::new();
+#[repr(C)]
+pub struct EdgesIter<'a> {
+    finished: bool,
+    iter:  Box<dyn Iterator<Item = Edge> + 'a>,
 }
 
-#[no_mangle]
-pub extern fn parse_gfa_into_hash_graph(file: &str) -> HashGraph {
-    let parser = GFAParser::new();
-    let gfa: GFA<usize, ()> = parser.parse_file(file).unwrap();
-    let hashgraph = HashGraph::from_gfa(&gfa);
-    return hashgraph;
+#[repr(C)]
+pub struct PathsIter<'a> {
+    finished: bool,
+    iter:  Box<dyn Iterator<Item = &'a i64> + 'a>,
 }
 
-// #[no_mangle]
-// pub extern fn parse_gfa_into_path_graph(file: &str) -> HashGraph {
-//     let parser = GFAParser::new();
-//     let gfa: GFA<usize, ()> = parser.parse_file(file).unwrap();
-//     let hashgraph = conversion::from_gfa::<PathGraph, _>(&gfa);
-//     return hashgraph;
-// }
+impl CGraph {
+    pub fn parse_gfa(file: &str) -> Self {
+        let parser = GFAParser::new();
+        let gfa: GFA<usize, ()> = parser.parse_file(file).unwrap();
+        let hashgraph = HashGraph::from_gfa(&gfa);
+        CGraph { graph: hashgraph }
+    }
 
-#[no_mangle]
-pub extern fn handle_graph_max_node_id(graph: &dyn HandleGraph) -> NodeId {
-    return graph.max_node_id();
-}
+    pub fn handles(&self) -> HandlesIter<'_> {
+        let iter = self.graph.handles_iter();
+        HandlesIter {
+            finished: false,
+            iter,
+        }
+    }
 
-#[no_mangle]
-pub extern fn handle_graph_min_node_id(graph: &dyn HandleGraph) -> NodeId {
-    return graph.min_node_id();
-}
+    pub fn edges(&self) -> EdgesIter<'_> {
+        let iter = self.graph.edges_iter();
+        EdgesIter {
+            finished: false,
+            iter,
+        }
+    }
 
-#[no_mangle]
-pub extern fn handle_graph_has_node(graph: &dyn HandleGraph, node_id: NodeId) -> bool {
-    return graph.has_node(node_id);
-}
-
-#[no_mangle]
-pub extern fn handle_graph_sequence_length_of_node(graph: &dyn HandleGraph, handle: Handle) -> usize {
-    return graph.length(handle);
-}
-
-#[no_mangle]
-pub extern fn handle_graph_node_count(graph: &dyn HandleGraph) -> usize {
-    return graph.node_count();
-}
-
-#[no_mangle]
-pub extern fn handle_graph_edge_count(graph: &dyn HandleGraph) -> usize {
-    return graph.edge_count();
-}
-
-#[no_mangle]
-pub extern fn handle_graph_has_edge(graph: &dyn HandleGraph, left: Handle, right: Handle) -> bool {
-    return graph.has_edge(left, right);
-}
-
-#[no_mangle]
-pub extern fn handle_graph_handles_iterator<'a>(graph: &'a dyn HandleGraph) -> Box<dyn Iterator<Item = Handle> + 'a> {
-    return graph.handles_iter();
-}
-
-#[no_mangle] 
-pub extern fn handles_iterator_next(mut iterator: Box<dyn Iterator<Item = Handle>>) -> Option<Handle> {
-    return iterator.next();
-}
-
-#[no_mangle] 
-pub extern fn handles_iterator_isdone(handle: Option<Handle>) -> bool {
-    return None != handle;
-}
-
-#[no_mangle] 
-pub extern fn handles_from_option(option: Option<Handle>) -> Handle {
-    match option {
-        Some(p) => return p,
-        None => panic!("has no value"),
+    pub fn paths(&self) -> PathsIter<'_> {
+        let iter = self.graph.paths.keys();
+        PathsIter {
+            finished: false,
+            iter: Box::new(iter),
+        }
     }
 }
 
 #[no_mangle]
-pub extern fn handle_graph_edges_iterator<'a>(graph: &'a dyn HandleGraph) -> Box<dyn Iterator<Item = Edge> + 'a> {
-    return graph.edges_iter();
+pub unsafe extern "C" fn load_hashgraph(file_name: *const u8, file_name_len: usize) -> *mut CGraph {
+    let file_name_slice = std::slice::from_raw_parts(file_name, file_name_len);
+    let file_str = std::str::from_utf8(file_name_slice).unwrap();
+    Box::into_raw(Box::new(CGraph::parse_gfa(file_str)))
 }
 
-#[no_mangle] 
-pub extern fn edges_iterator_next(mut iterator: Box<dyn  Iterator<Item = Edge>>) -> Option<Edge> {
-    return iterator.next();
+#[no_mangle]
+pub unsafe extern "C" fn free_hashgraph(graph: *mut CGraph) {
+    let _ = Box::from_raw(graph);
 }
 
-#[no_mangle] 
-pub extern fn edges_iterator_isdone(edge: Option<Edge>) -> bool {
-    return None != edge;
+#[no_mangle]
+pub unsafe extern "C" fn graph_handles<'a>(graph: *const CGraph) -> *mut HandlesIter<'a> {
+    let g = &(*graph);
+    let iter = g.handles();
+    Box::into_raw(Box::new(iter))
 }
 
-#[no_mangle] 
-pub extern fn edge_from_option(option: Option<Edge>) -> Edge {
-    match option {
-        Some(p) => return p,
-        None => panic!("has no value"),
+#[no_mangle]
+pub unsafe extern "C" fn free_handles_iter<'a>(handles: *mut HandlesIter<'a>) {
+    let _ = Box::from_raw(handles);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn handles_next<'a>(handles: *mut HandlesIter<'a>) -> u64 {
+    let handles = &mut (*handles);
+    let next = handles.iter.next();
+    match next {
+        None => {
+            handles.finished = true;
+            0
+        }
+        Some(h) => u64::from(h.id()),
     }
 }
 
 #[no_mangle]
-pub extern fn handle_edges_iter_right<'a>(graph: &'a dyn HandleGraph, handle: Handle) -> Box<dyn Iterator<Item = Handle> + 'a> {
-    return graph.handle_edges_iter(handle, Direction::Right);
+pub unsafe extern "C" fn handles_has_next<'a>(handles: *mut HandlesIter<'a>) -> bool {
+    let handles = &mut (*handles);
+    return handles.finished;
 }
 
 #[no_mangle]
-pub extern fn handle_edges_iter_left<'a>(graph: &'a dyn HandleGraph, handle: Handle) -> Box<dyn Iterator<Item = Handle> + 'a> {
-    return graph.handle_edges_iter(handle, Direction::Left);
+pub unsafe extern "C" fn graph_edges<'a>(graph: *const CGraph) -> *mut EdgesIter<'a> {
+    let g = &(*graph);
+    let iter = g.edges();
+    Box::into_raw(Box::new(iter))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_edges_iter<'a>(handles: *mut EdgesIter<'a>) {
+    let _ = Box::from_raw(handles);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn edges_next<'a>(edges: *mut EdgesIter<'a>) -> EdgeHolder {
+    let edges = &mut (*edges);
+    let next = edges.iter.next();
+    match next {
+        None => {
+            edges.finished = true;
+            return EdgeHolder{left:0, right:0};
+        }
+        Some(e) => EdgeHolder{left: u64::from(e.0.id()) , right: u64::from(e.1.id())},
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn edges_has_next<'a>(edges: *mut EdgesIter<'a>) -> bool {
+    let edges = &mut (*edges);
+    return edges.finished;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn graph_paths<'a>(graph: *const CGraph) -> *mut PathsIter<'a> {
+    let g = &(*graph);
+    let iter = g.paths();
+    Box::into_raw(Box::new(iter))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_paths_iter<'a>(paths: *mut PathsIter<'a>) {
+    let _ = Box::from_raw(paths);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn paths_next<'a>(paths: *mut PathsIter<'a>) -> i64 {
+    let paths = &mut (*paths);
+    let next = paths.iter.next();
+    match next {
+        None => {
+            paths.finished = true;
+            return 0;
+        }
+        Some(e) => *e
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn paths_has_next<'a>(paths: *mut PathsIter<'a>) -> bool {
+    let paths = &mut (*paths);
+    return paths.finished;
 }
